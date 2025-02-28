081d3ff3ba6b94c151565262f7d446f7d2f70b9b
Amit-TheOne <amit.dew009@gmail.com>
2025-02-28 20:17:18 +0530

    add: Projects section and projects data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/sections/Hero";
 import { Navbar } from "@/sections/Navbar";
+import { Projects } from "@/sections/Projects";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Projects />
     </div>
   );
 }
