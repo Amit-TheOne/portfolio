0a1d4234f3d317f3bbc174ddeda4a33e0a0ad502
Amit-TheOne <amit.dew009@gmail.com>
2025-03-09 23:58:47 +0530

    refactor: changed projects.ts to projectsData.ts and update projectsData structure

---
+++ src/data/projectsData.ts
@@ -0,0 +1,24 @@
+import dummyImage from "@/assets/images/dummy.jpg";
+
+export const portfolioProjects = [
+    {
+        title: "Peoject Title 1",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc.",
+        previewLink: "https://google.com",
+        repoLink: "https://github.com",
+        image: dummyImage,
+        techUsed: [
+            "React",
+            "TypeScript",
+            "TailwindCSS",
+            "Next.js",
+            "Firebase",
+            "React",
+            "TypeScript",
+            "TailwindCSS",
+            "Next.js",
+            "Firebase",
+        ],
+    },
+];
