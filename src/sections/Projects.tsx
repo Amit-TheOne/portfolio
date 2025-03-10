8c128929e8f1345ab782e05b363a6389b14c0f84
Amit-TheOne <amit.dew009@gmail.com>
2025-03-10 16:16:23 +0530

    update: Projects section with projectsData structure

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-import grainImage from "@/assets/images/grain.jpg";
-import { portfolioProjects } from "@/data/projects";
+import { portfolioProjects } from "@/data/projectsData";
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 
@@ -16,43 +14,52 @@ export const Projects = () => {
                     description="See how I transformed concepts into engaging experiences."
                 />
 
-                {/* PROJECT CARD */}
-                <div className="mt-10 md:mt-20 flex flex-col gap-20">
+                <div className="mt-14 md:mt-20 flex flex-col gap-20">
                     {portfolioProjects.map((project) => (
                         <Card
                             key={project.title}
-                            className="px-8 pt-8 pb-0 md:pt-12 md:px-12 lg:pt-16 lg:px-20"
+                            className="px-8 pt-8 pb-0 md:pt-10 md:px-12 lg:px-14"
                         >
                             <div className="lg:grid lg:grid-cols-2 lg:gap-16">
-                                <div className="lg:pb-16">
-                                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                                        <span>{project.company}</span>
-                                        <span>&bull;</span>
-                                        <span>{project.year}</span>
-                                    </div>
-
-                                    <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">
+                                <div className="lg:pb-10">
+                                    <h3 className="font-serif text-2xl md:text-4xl">
                                         {project.title}
                                     </h3>
                                     <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
-                                    <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                                        {project.results.map((result) => (
-                                            <li
-                                                key={result.title}
-                                                className="flex gap-2 text-sm md:text-base text-white/50"
-                                            >
-                                                <CheckCircleIcon className="size-5 md:size-6" />
-                                                <span>{result.title}</span>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                    <a href={project.link}>
-                                        <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex justify-center items-center gap-2 mt-8">
-                                            <span>Visit Live Site</span>
-                                            <ArrowUpRightIcon className="size-4" />
-                                        </button>
-                                    </a>
+
+                                    <p className="text-sm md:text-base text-white/75 mt-4 md:mt-5">
+                                        {project.description}
+                                    </p>
+
+                                    <div className="flex flex-col md:flex-row gap-5 md:gap-6 mt-7">
+                                        <a
+                                            href={project.previewLink}
+                                            target="_blank"
+                                        >
+                                            <button className="bg-white text-gray-950 h-10 md:h-12 w-full md:w-auto px-8 rounded-xl font-semibold inline-flex justify-center items-center gap-3">
+                                                <span>View Project</span>
+                                                <ArrowUpRightIcon className="size-4" />
+                                            </button>
+                                        </a>
+                                        <a
+                                            href={project.repoLink}
+                                            target="_blank"
+                                        >
+                                            <button className="bg-white text-gray-950 h-10 md:h-12 w-full md:w-auto px-8 rounded-xl font-semibold inline-flex justify-center items-center gap-3">
+                                                <span>View Repo</span>
+                                                <ArrowUpRightIcon className="size-4" />
+                                            </button>
+                                        </a>
+                                    </div>
+                                    <hr className="border-t-2 border-white/5 mt-6 md:mt-7" />
+
+                                    <div className="mt-4 md:mt-5">
+                                        <span className="bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text font-semibold tracking-wide md:tracking-wider uppercase text-xs md:text-sm ">
+                                            {project.techUsed.join(" • ")}
+                                        </span>
+                                    </div>
                                 </div>
+
                                 <div className="realtive">
                                     <Image
                                         src={project.image}
