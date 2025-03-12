c1033b2ea2ce7888fc40271072135e2f1f9d793f
Amit-TheOne <amit.dew009@gmail.com>
2025-03-12 20:29:17 +0530

    update: projectsData with projects and its links

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -2,23 +2,59 @@ import dummyImage from "@/assets/images/dummy.jpg";
 
 export const portfolioProjects = [
     {
-        title: "Peoject Title 1",
+        title: "Evo Gym",
         description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc.",
-        previewLink: "https://google.com",
-        repoLink: "https://github.com",
+            "A dynamic landing page for Evo Gym, showcasing its state-of-the-art facilities, expert trainers, and diverse fitness classes. Users can explore the gym, book a free trial session, and easily get in touch for more details.",
+        previewLink: "https://evo-gym-fitness.netlify.app",
+        repoLink: "https://github.com/Amit-TheOne/EvoGym",
         image: dummyImage,
-        techUsed: [
-            "React",
-            "TypeScript",
-            "TailwindCSS",
-            "Next.js",
-            "Firebase",
-            "React",
-            "TypeScript",
-            "TailwindCSS",
-            "Next.js",
-            "Firebase",
-        ],
+    },
+    {
+        title: "Homzy",
+        description:
+            "A modern real estate landing page that connects buyers and sellers with ease. Homzy offers a diverse range of properties, from stylish apartments to luxurious villas, allowing users to browse, filter, and connect seamlessly with agents.",
+        previewLink: "https://homzy-estate.netlify.app",
+        repoLink: "https://github.com/Amit-TheOne/Homzy",
+        image: dummyImage,
+    },
+    {
+        title: "iNotes",
+        description:
+            "A MERN stack-powered note-taking app designed for simplicity and efficiency. iNotes enables users to create, edit, and delete notes while ensuring secure access through user authentication. With a clean and intuitive interface, it helps users stay organized effortlessly.",
+        previewLink: "https://inotes-notesapp.netlify.app/login",
+        repoLink: "https://github.com/Amit-TheOne/iNotes",
+        image: dummyImage,
+    },
+    {
+        title: "ZenFi",
+        description:
+            "An all-in-one virtual productivity space designed to enhance focus and efficiency. Zenfi seamlessly integrates task management, a Pomodoro timer, LoFi music, scenic views, and ZenAI into a single interface. Users can create tasks, set focused work sessions, and stay in the zone with immersive visuals and calming sounds without the need to switch tabs making it the ultimate workspace for deep concentration.",
+        previewLink: "",
+        repoLink: "https://github.com/Amit-TheOne/ZenFi",
+        image: dummyImage,
+    },
+    {
+        title: "Income-Expense Tracker",
+        description:
+            "A user-friendly web application for managing finances effortlessly. Users can add, delete, and categorize transactions, track their balance, and analyze spending patterns with interactive graphs. Powered by Speechly, the app also allows voice commands for quick transaction entries, enhancing convenience and efficiency.",
+        previewLink: "https://income-expense-trackerapp.netlify.app",
+        repoLink: "https://github.com/Amit-TheOne/Income-Expense-Tracker",
+        image: dummyImage,
+    },
+    {
+        title: "DATAEYE",
+        description:
+            "A sleek landing page for DataEye, a powerful data analytics platform designed to help businesses track, analyze, and visualize key metrics in real-time. With an intuitive dashboard, customizable reports, and predictive insights, DataEye empowers B2B, B2C, and SaaS platforms to make data-driven decisions and drive growth.",
+        previewLink: "https://dataeye.netlify.app/",
+        repoLink: "https://github.com/Amit-TheOne/DATAEYE",
+        image: dummyImage,
+    },
+    {
+        title: "IoT - Distance Measurement System",
+        description:
+            "An IoT-powered distance measurement system utilizing Arduino Uno and an Ultrasonic Sensor to calculate the distance between the sensor and an object. The measured distance is displayed in both centimeters and inches on an LCD screen, making it a practical solution for various applications.",
+        previewLink: "https://www.youtube.com/watch?v=cBoGD3gLO2A",
+        repoLink: "https://github.com/Amit-TheOne/IoT-Project",
+        image: dummyImage,
     },
 ];
