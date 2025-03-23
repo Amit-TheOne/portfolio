25bc2fa3beb4b8001f1467d68ad83821fa575f87
Amit-TheOne <amit.dew009@gmail.com>
2025-03-23 21:18:25 +0530

    update: replace a tags with Next.js Link

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,13 +1,25 @@
+import Link from "next/link";
+
 export const Navbar = () => {
-  return (
-    <div className="flex justify-center items-center fixed top-3 w-full z-10">
-      <nav className=" flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#hero" className="nav-item">Home</a>
-        <a href="#about" className="nav-item">About</a>
-        <a href="#projects" className="nav-item">Projects</a>
-        <a href="#skills" className="nav-item">Skills</a>
-        <a href="#contact" className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900">Contact</a>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center fixed top-3 w-full z-10">
+            <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+                <Link href="#hero" className="nav-item">
+                    Home
+                </Link>
+                <Link href="#about" className="nav-item">
+                    About
+                </Link>
+                <Link href="#projects" className="nav-item">
+                    Projects
+                </Link>
+                <Link href="#skills" className="nav-item">
+                    Skills
+                </Link>
+                <Link href="#contact" className="nav-item">
+                    Contact
+                </Link>
+            </nav>
+        </div>
+    );
 };
