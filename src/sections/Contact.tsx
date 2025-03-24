00920d93ab475bf8e1b65ca04842cc30a4e4c319
Amit-TheOne <amit.dew009@gmail.com>
2025-03-24 23:51:33 +0530

    update: changed contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,10 +1,10 @@
 import { SectionHeader } from "@/components/SectionHeader";
-import { Card } from "@/components/Card";
-import { socialData } from "@/data/socialData";
+import grainImage from "@/assets/images/grain.jpg";
+import { ContactButton } from "@/components/ContactButton";
 
 export const Contact = () => {
     return (
-        <section className="py-12 md:py-16 lg:py-24">
+        <section id="contact" className="py-12 md:py-16 lg:py-24">
             <div className="container">
                 <SectionHeader
                     eyebrow="Want to reach out?"
@@ -12,53 +12,28 @@ export const Contact = () => {
                     description="Connect with me on my socials or email me directly to reach out."
                 />
 
-                <div className="mt-16 md:mt-20 lg:mt-24 gap-8">
-                    <Card className="p-0 flex flex-col ">
-                        <nav className="flex flex-row gap-8 md:gap-12 lg:gap-14 items-center flex-wrap px-4 md:px-10 lg:px-24 pt-10 text-base md:text-lg font-medium">
-                            {socialData.slice(0, 5).map((link) => {
-                                const Component = link.iconType;
-
-                                return (
-                                    <a
-                                        key={link.title}
-                                        href={link.href}
-                                        target="blank"
-                                        className="inline-flex items-center gap-4 px-4 md:px-8 py-2 md:py-3 text-gray-900 hover:text-white fill-gray-900 hover:fill-white bg-gradient-to-r from-emerald-300 to-sky-400 outline outline-2 outline-transparent hover:bg-gradient-to-r hover:from-emerald-300/70 hover:to-sky-400/70 hover:outline-white rounded-xl mx-auto transition-all duration-[400ms] hover:cursor-pointer"
-                                    >
-                                        <Component className="size-5" />
-                                        <span className="">{link.title}</span>
-                                    </a>
-                                );
-                            })}
-                        </nav>
-
-                        <nav className="flex items-center px-4 md:px-10 lg:px-24 pt-8 md:pt-12 lg:pt-14 pb-10 mx-auto my-auto text-base md:text-lg font-medium">
-                            {socialData.slice(5).map((link) => {
-                                const Component = link.iconType;
-
-                                return (
-                                    <div
-                                        key={link.title}
-                                        className="flex flex-col md:flex-row items-center gap-4 px-4 md:px-8 py-2 md:py-3 text-gray-900 hover:text-white fill-gray-900 hover:fill-white bg-gradient-to-r from-emerald-300 to-sky-400 outline outline-2 outline-transparent hover:bg-gradient-to-r hover:from-emerald-300/70 hover:to-sky-400/70 hover:outline-white rounded-xl mx-auto transition-all duration-[400ms] hover:cursor-default"
-                                    >
-                                        <div className="inline-flex items-center gap-4 mx-auto">
-                                            <Component className="size-5" />
-                                            <span className="">
-                                                {link.title}
-                                            </span>
-                                        </div>
-
-                                        <div className="flex text-wrap items-center gap-2 relative z-0 px-3 ml-3">
-                                            <div className="absolute inset-0 z-10 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-300  to-cyan-900 hover:bg-none rounded-2xl blur-sm -mx-2 -my-1 transition-all duration-1000 cursor-text hover:select-text" />
-                                            <span className="">
-                                                {link.href}
-                                            </span>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                        </nav>
-                    </Card>
+                <div className="mt-16 md:mt-20">
+                    <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+                        <div
+                            className="absolute inset-0 opacity-10 -z-10"
+                            style={{
+                                backgroundImage: `url(${grainImage.src})`,
+                            }}
+                        />
+                        <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+                            <div>
+                                <h2 className="font-serif text-2xl md:text-3xl">
+                                    {`Let's create something amazing together`}
+                                </h2>
+                                <p className="text-sm md:text-base mt-2 font-medium">
+                                    {`Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.`}
+                                </p>
+                            </div>
+                            <div>
+                                <ContactButton />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
